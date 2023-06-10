2d1320297a54f86fbca447680c56e7e925c24ed7
GolosCD <ananev@asta>
2023-06-10 12:37:37 +0300

    Значение многочлена 🌶️

---
+++ 15.2.5.py
@@ -0,0 +1,20 @@
+from math import sin
+
+#квадрат
+def pow (num:int):
+    return num**2
+#куб
+def cube (num:int):
+    return num**3
+
+#корень
+def srt (num:int):
+    return num**0.5
+
+#модуль
+def abss (num:int):
+    return abs(num)
+
+#синус
+def sun (num:int):
+    return sin(num)
